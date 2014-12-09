b29fa11697ea6175a6c25352b935a9364b80f53c
Jose Ignacio <joserobleda@gmail.com>
2014-12-09 18:12:36 +0100

    update 09/12

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,40 @@
-var app = angular.module('dokifyApp', []);
+var app = angular.module('dokifyApp', [
+	'ngRoute',
+	'appControllers'
+]);
 
+app.run(function ($rootScope) {
+	$rootScope.$on('$routeChangeStart', function () {
+		console.log('route change start');
+	});
 
+	$rootScope.$on('$routeChangeError', function () {
+		console.log('route change error');
+	});
 
+	$rootScope.$on('$routeChangeSuccess', function () {
+		console.log('route change success');
+	});
 
-app.controller('CompanyCtrl', function ($scope) {
-	var company = {};
-
-	company.name 		= 'dokify';
-	company.employees 	= [
-		{name: 'pepe', vat: '1234'}
-	];
-
-	$scope.company = company;
+	console.log('listo!');
 });
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/company', {
+			templateUrl: '/html/company.html',
+			controller: 'CompanyCtrl'
+		}).
+		when('/employee', {
+			templateUrl: '/html/employee.html',
+			controller: 'EmployeeCtrl',
+			resolve: {
+				employeePromise: function ($http) {
+					return $http.get('/employee.json');
+				}
+			}
+		}).
+		otherwise({
+			redirectTo: '/company'
+		});
+}]);
